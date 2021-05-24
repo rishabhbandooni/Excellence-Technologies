9ac0fe06861ef4641f5cbdef5f843329a13c82e4
RISHABH BANDOONI <bandooni.rishabh99@gmail.com>
2021-05-24 15:33:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function Todo({ todo, index,addTodo}) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      
+    >
+      
+      {todo.text}
+    
+    </div>
+    
+  );
+}
+
+function TodoForm({ addTodo ,index }) {
+  const [value, setValue] = React.useState("");
+ 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return  (
+    
+
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Complete the React js logical test",
+     
+    },
+    {
+      text: "Make a to-do app",
+      
+    },
+    {
+      text: "React js final logical test completed",
+     
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+    
+  };
+
+  
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+           
+          />
+        ))}
+          
+        <TodoForm addTodo={addTodo} />
+      
+      </div>
+    
     </div>
   );
 }
